0fc0aa8183de3da063d88009fa6ed2476713cf1a
Paul Cozzi <Paul Cozzi>
2016-10-12 21:17:33 -0500

    Started editing header

--- assignment4/Header.java
+++ assignment4/Header.java
@@ -2,12 +2,12 @@ package assignment4;
 /* CRITTERS <MyClass.java>
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
+ * Paul Cozzi
+ * pac2472
+ * 16450
+ * Mina Sucur
  * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * 16450
  * Slip days used: <0>
  * Fall 2016
  */
