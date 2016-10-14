4f84882abb790d114e8e2e4d504feb9eb1308e5c
Mina Sucur <minasucur@wireless-10-145-114-60.public.utexas.edu>
2016-10-14 14:42:25 -0500

    hi

---
+++ assignment4/Header.java
@@ -0,0 +1,13 @@
+package assignment4;
+/* CRITTERS <MyClass.java>
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Paul Cozzi
+ * pac2472
+ * 16450
+ * Mina Sucur
+ * <Student2 EID>
+ * 16450
+ * Slip days used: <0>
+ * Fall 2016
+ */
