46ba734c6c1318f7f18d96c0124a99cff448c9e1
Paul Cozzi <Paul Cozzi>
2016-10-17 13:28:04 -0500

    changes

--- assignment4/Critter3.java
+++ assignment4/Critter3.java
@@ -14,11 +14,16 @@ package assignment4;
 /*Critter3 is a trapping critter
  * it does not move during timeSteps
  * it will always elect to fight, except when ecountered with a Critter3, when it will walk away
- * it will always reproduce in doTimeStep if it has enough energy
+ * it will always reproduce in doTimeStep if it has enough energy (above its own genetic energy limit)
  */
 
 public class Critter3 extends Critter {
 	private int reproduceLimit = Params.min_reproduce_energy * 5;
+	/**
+	 * reproduces if over the energy limit for this specific critter
+	 * @param none
+	 * @return none
+	 */
 	@Override
 	public void doTimeStep() {
 		//want to be a good fighter, reproduce in timestep only if over the limit
@@ -29,7 +34,11 @@ public class Critter3 extends Critter {
 		}
 		
 	}
-
+	/**
+	 * will fight anything but Critter3
+	 * @param none
+	 * @return true if critter3, false otherwise
+	 */
 	@Override
 	public boolean fight(String oponent) {
 		//Critter3 is a fighter
